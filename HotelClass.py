2106a4cc08058df42e98229bc24b3494416df05b
charutapande <charuta.pande@fhnw.ch>
2024-03-18 00:25:31 +0100

    L4

---
+++ HotelClass.py
@@ -0,0 +1,12 @@
+class Hotel:
+    # Define a constructor and initialize attributes as instance variables
+    def __init__(self, name, city, no_of_rooms, stars, is_available, price_per_night):
+        self.hotel_name = name
+        self.city = city
+        self.no_of_rooms = no_of_rooms
+        self.stars = stars
+        self.is_available = is_available
+        self.price_per_night = price_per_night
+
+
+
